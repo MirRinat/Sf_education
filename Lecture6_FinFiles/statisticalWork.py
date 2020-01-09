1ad2ad8c4d9ca80ed57c9bb4f2d589b1dd4a4acb
Bonebreaker Glinda <bglinda@mi-d6.21-school.ru>
2020-01-09 19:02:11 +0300

    The first part of determining the price of Microsoft stocks taking into account inflation

--- Lecture6_FinFiles/statisticalWork.py
+++ Lecture6_FinFiles/statisticalWork.py
@@ -5,46 +5,26 @@ import matplotlib.pyplot as plt
 from pandas_datareader import wb
 import quandl
 
-#Загрузить акции Майкрософт
-startDate = '2013-1-1'
+#####download Microsoft's stocks#####
+startDate = '2015-1-1'
 endDate = dt.datetime.today()
-
-MSFT = web.DataReader('MSFT','google',startDate, endDate)
+MSFT = web.DataReader("MSFT",'yahoo',startDate,endDate)
 MSFTSelected = MSFT['Close']
-#print(MSFTSelected)
 
-stDev = MSFT['Close'].std()
-avg = MSFT['Close'].mean()
+stDev = MSFTSelected.std() #среднее квадратичное отклонение
+avg = MSFTSelected.mean() #среднее значение
 
-inflation = web.DataReader('CPIAUCSL','fred',startDate,endDate)
-inflation.rename(index=str, columns={'CPIAUCSL' : 'Value'}, inplace=True)
+print("stDev = ",stDev)
+print("avg = ", avg)
 
-inflation = inflation.assign( Prev = inflation['Value'].shift(1) )
-
-
-def calculateTheDiff(row):
-    return row['Value']/row['Prev'] - 1
-
-inflation['pctChange0'] = inflation.apply(lambda row: calculateTheDiff(row), axis=1)
+#MSFTOneSigma = MSFT[MSFT['Close'] > (MSFT['Close'].mean() + 2.4 * stDev)]
+#print('MSFTOneSigma = ',MSFTOneSigma)
 
 
+iflation = web.DataReader('CPIAUCSL','fred',startDate,endDate)
+iflation.rename(index=str, columns={'CPIAUCSL' : 'Value'}, inplace=True)#переименование столбца CPIAUCSL в Value
 
-inflation['pctChange1'] = inflation.apply(lambda row: (row['Value']/row['Prev'] - 1), axis=1)
-
-
-#=B3/B2-1
-inflation['pctChange2'] = inflation.apply(lambda row: (row['Value']/row.shift(-1)['Value'] -1), axis=1)
-
-
-
-inflation = inflation.assign( pctChange3 = inflation['Value'].pct_change())
-
-#=$B$62/B3
-inflation['currentPrice'] = inflation.apply(lambda row: (inflation.tail(1)['Value'] / row['Value']), axis = 1)
-inflation.drop(['Prev', 'pctChange0', 'pctChange1', 'pctChange2', 'pctChange3'],axis=1,inplace = True)
-
-pricesWInflation = inflation.join(MSFT['Close'])
-pricesWInflation['priceInCurrentDollars'] = pricesWInflation['currentPrice'] * pricesWInflation['Close']
-
-pricesWInflation[['priceInCurrentDollars','Close']].plot()
-plt.show()
+monthSD = pd.date_range(startDate,endDate,freq='MS') #оставляет только значения,которые сооствествуют началу месяца
+MSFT = MSFT.reindex(monthSD)#меняет индекс на новые даты
+MSFT = MSFT.dropna() #удаляет значения с NAN т.е нерабочие дни каждого первого числа месяца
+print(MSFT)
