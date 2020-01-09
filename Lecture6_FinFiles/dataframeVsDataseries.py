c4b563b756e1c724e1d309dbc5727341ae4eacba
Bonebreaker Glinda <bglinda@mi-d6.21-school.ru>
2020-01-09 14:05:16 +0300

    Разница DataFrame и dataSeries

--- Lecture6_FinFiles/dataframeVsDataseries.py
+++ Lecture6_FinFiles/dataframeVsDataseries.py
@@ -6,5 +6,11 @@ fromDate = '2011-04-30'
 toDate = '2018-06-15'
 
 dataFrame = web.DataReader("GS",'yahoo',fromDate,toDate)
-print(dataFrame.head())
+dataSeries = dataFrame['Close']
+dataFrameOne = dataFrame[['Close']]
+dataFrameOne['CloseInRub'] = dataFrameOne['Close'] * 66
+print(dataSeries.head())
+print(dataFrameOne.head())
 
+#Разница между dataframe и dataSeries - два разных объекта,с одинковыми данными,но разным функционалом
+#dataFrame - это коллекция,массив данных,состоящий из dataseries
