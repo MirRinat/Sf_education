f3504c6c22cfcd1953eeaa98addaf7d05f60eec8
mirrinat <mirzagalamov.rinat1997@mail.ru>
2020-01-11 13:09:15 +0300

    готово изменение цены акций Майкрософта с учетом инфляции с 2015года

--- Lecture6_FinFiles/statisticalWork.py
+++ Lecture6_FinFiles/statisticalWork.py
@@ -1,3 +1,6 @@
+# coding=<utf-8>
+# -*- coding: <utf-8> -*-
+# vim: set fileencoding=<utf-8> :
 import pandas_datareader.data as web
 import pandas as pd
 import datetime as dt
@@ -21,10 +24,29 @@ print("avg = ", avg)
 #print('MSFTOneSigma = ',MSFTOneSigma)
 
 
-iflation = web.DataReader('CPIAUCSL','fred',startDate,endDate)
-iflation.rename(index=str, columns={'CPIAUCSL' : 'Value'}, inplace=True)#переименование столбца CPIAUCSL в Value
+inflation = web.DataReader('CPIAUCSL', 'fred', startDate, endDate)
+inflation.rename(index=str, columns={'CPIAUCSL' : 'Value'}, inplace=True)#переименование столбца CPIAUCSL в Value
 
 monthSD = pd.date_range(startDate,endDate,freq='MS') #оставляет только значения,которые сооствествуют началу месяца
 MSFT = MSFT.reindex(monthSD)#меняет индекс на новые даты
-MSFT = MSFT.dropna() #удаляет значения с NAN т.е нерабочие дни каждого первого числа месяца
-print(MSFT)
+MSFT = MSFT.fillna(method = 'ffill') #заменяет NAN на значения value,но method = ffill заменяет предыдущие значения в строках выше
+#MSFT = MSFT.dropna() #удаляет значения с NAN т.е нерабочие дни каждого первого числа месяца
+#print(MSFT)
+inflation = inflation.assign(Prev = inflation['Value'].shift(1))#создание новой колонки
+
+def calculateTheDiff(row):
+    return row['Value']/row['Prev'] - 1
+
+inflation['pctChange0'] = inflation.apply(lambda row:calculateTheDiff(row), axis=1)#определение процентного изменения с предыдущим показателем
+inflation['pctChange1'] = inflation.apply(lambda row: (row['Value'] / row.shift(-1)['Value'] - 1), axis=1)#определение процентного изменения с предыдущим показателем методом lambda
+inflation = inflation.assign(pctChange2 = inflation['Value'].pct_change())#определение процентного измененияс предыдущим показателем методом из библиотеки pandas
+inflation.to_excel('recData/chngInflation.xlsx')
+
+inflation.drop(['Prev', 'pctChange0', 'pctChange1', 'pctChange2'], axis=1, inplace=True)#удаление столбцов
+inflation['currentPrice'] = inflation.apply(lambda row:inflation.tail(1)['Value']/row['Value'],axis=1)
+
+pricesWithInflation = inflation.join(MSFT['Close'])
+pricesWithInflation['priceInCurrentDollars'] = pricesWithInflation['currentPrice'] * pricesWithInflation['Close']
+pricesWithInflation[['priceInCurrentDollars','Close']].plot()
+plt.show()
+
