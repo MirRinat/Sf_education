19f58be9e22602786d51cad23df2158a708d76d5
Bonebreaker Glinda <bglinda@mi-d6.21-school.ru>
2020-01-15 14:37:59 +0300

    HW6

---
+++ Homework6/HW6.py
@@ -0,0 +1,29 @@
+import pandas_datareader as wb
+import datetime as date
+
+#Шаг 1(10 баллов): Удалено скачайте "Treasury Constant Maturity Rate" с сайта фред FRED
+#(https://fred.stlouisfed.org/categories/115) от 02/01/2014 до 02/01/2016 для следующих кривых:
+#6-Month
+#1-Year
+#5-Year
+#10-Year
+start = '2014-01-02'
+end = '2016-01-02'
+tickers = ["DGS6MO","DGS1","DGS5","DGS10"]
+dgs = wb.DataReader(tickers,'fred',start,end)
+dgs = dgs.dropna()
+#Шаг 2( 5 баллов ): Определите среднее и стандартную дивиацию для каждой из кривых( кривая это 6-month, 1 year, и т.д.)
+stdDev = dgs.std()
+avg = dgs.mean()
+print("Среднее отклонение\n",stdDev)
+print("Стандартное отклонение\n", avg)
+# for tic in tickers:
+#     stdDict = {tic: dgs[tic].std()}
+#     avgDict = {tic: dgs[tic].mean()}
+#     print("Стандартное отклонение",tic, stdDict[tic])
+#     print("Среднее отклонение",tic, avgDict[tic])
+
+#Шаг 3( 5 баллов ): Выберете только те даты которые имеют показателе выше или ниже avg +/- 1 std
+dgsOneSigma = dgs[(dgs > (avg + stdDev)) | (dgs < (avg - stdDev))]
+
+print(dgsOneSigma)
